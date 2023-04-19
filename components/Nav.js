b6fc662e602c69a36097261d4d006833f91ce9b8
amir-mrd <amirm751@gmail.com>
2023-04-19 21:45:02 +1000

    add click event to darkmode

--- components/Nav.js
+++ components/Nav.js
@@ -9,7 +9,7 @@ const Nav = ({setDarkMode,darkMode}) => {
       <nav className="py-10 mb-2 flex justify-between dark:text-white">
         <h1 className='text-2xl font-burtons'>Amir Mohammadi</h1>
         <ul className='flex items-center'>
-          <li><CgDarkMode className='cursor-pointer text-2xl'/></li>
+          <li><CgDarkMode onClick={()=>setDarkMode(!darkMode)} className='cursor-pointer text-2xl'/></li>
           <li><a href="https://drive.google.com/file/d/1UOvVPnAENOzbUQo1Xi7wzU4RC5eVM2So/view?usp=sharing" target="_blank" className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8'>Resume</a></li>
           
           <li className='text-5xl flex justify-center gap-16 py-3 text-teal-600 '>
