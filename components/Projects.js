3198f4f70e2fda912e9cde3f86d0cc622102b80f
amir-mrd <amirm751@gmail.com>
2023-04-23 20:43:19 +1000

    corrections

--- components/Projects.js
+++ components/Projects.js
@@ -14,8 +14,9 @@ const Projects = () => {
       <h2 className='text-3xl text-gray-800 py-1 dark:text-white font-bold'>Sample Projects</h2>  
         <div className='md:grid md:grid-cols-2 gap-10'>
           <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1'>
-            <Image src={Wine_quaity} alt=''/>
-            <h3 className='text-2xl font-semibold pt-8 pb-2'>Wine Quality Prediction</h3>
+            <a href='https://github.com/amir-mrd/Wine-Quality-Prediction-Random-Forest' target="_blank"></a>
+              <Image src={Wine_quaity} alt=''/>
+              <h3 className='text-2xl font-semibold pt-8 pb-2'>Wine Quality Prediction</h3>
             <p className='py-2'>A Random Forest Classifier model to predict the quality of white wine (vinho verde) based on several features.
             .</p> 
           </div>
@@ -56,7 +57,7 @@ const Projects = () => {
       <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1'>
         <a href='https://github.com/amir-mrd/BOM-Rainfall-Data/' target="_blank">
         <Image src={rainfall} alt=''/>
-        <h3 className='text-2xl font-semibold pt-8 pb-2'>What's the weatheer like!</h3>
+        <h3 className='text-2xl font-semibold pt-8 pb-2'>What's the weather like!</h3>
         </a>
         <p className='py-2'>Analysing the BOM data for Brisbane and Sydney to predict the chance of rain.</p>
         
