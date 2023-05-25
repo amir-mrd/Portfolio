c0804279cb600e41fe5c62611186c858d19772a3
amir-mrd <amirm751@gmail.com>
2023-05-25 10:58:12 +1000

    name change

--- components/About.js
+++ components/About.js
@@ -13,7 +13,7 @@ const About = () => {
                </div>
                <div className='text-left ml-20 basis-1/2'>
                     <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-5xl dark:text-teal-400'>Amir Mohammadi</h2>
-                    <h3 className='text-2xl py-2 md:text-2xl dark:text-white'>Data Analyst</h3>
+                    <h3 className='text-2xl py-2 md:text-2xl dark:text-white'>Data Scientist</h3>
                     <p className='text-md py-5 text-gray-800 leading-8 md:text-xl md:px-2 max-w-3xl mx-auto dark:text-gray-200 tracking-wide '>A dedicated and resourceful data scientist, I am committed to utilizing data-driven insights to support informed business
 decisions and actions. With a strong background in Statistical Modeling, Machine Learning, Predictive Modeling, Data Mining,
 and SQL, I have successfully applied his analytical skills to a variety of projects across industries such as Energy, IT,
